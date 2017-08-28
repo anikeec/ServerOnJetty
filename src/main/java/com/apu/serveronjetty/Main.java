e69f3375c538320968368d72eba70848b1c959f9
apu <pasha_anik@ukr.net>
2017-08-28 11:34:04 +0300

    init state

---
+++ src/main/java/com/apu/serveronjetty/Main.java
@@ -0,0 +1,15 @@
+package com.apu.serveronjetty;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author apu
+ */
+public class Main {
+    
+}
